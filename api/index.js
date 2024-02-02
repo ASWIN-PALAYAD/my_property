0f06d882bb215e648a701516b8e5840d19b2f112
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-02-02 20:28:36 +0530

    api cors setting

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ import listingRouter from './routes/listingRoutes.js';
 const app = express();
 app.use(cors(
     {
-        origin: 'http://127.0.0.1:5173',
+        origin: 'https://myproperty.vercel.app',
     // origin: 'https://myproperty.vercel.app',
   methods: ["POST", "GET", "PUT", "DELETE"],
   credentials: true
